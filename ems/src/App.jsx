8cede4e288d99f4bccb034a7388ab1571c49af30
asna sharin <asnasherin1233@gmail.com>
2024-05-08 23:18:22 +0530

    dashboard design

--- ems/src/App.jsx
+++ ems/src/App.jsx
@@ -3,6 +3,10 @@ import Login from './components/Login'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
 import SignupPage from './components/Signup'
+import Dashboard from './components/Dashboard'
+import Employee from './components/Employee'
+import Category from './components/Category'
+import Profile from './components/Profile'
 
 function App() {
 
@@ -11,7 +15,11 @@ function App() {
    <Routes>
     <Route path='/signup' element={<SignupPage />} />
     <Route path='/login' element={<Login />} />
+    <Route path='/dashboard' element={<Dashboard />} />
     <Route path='/' element={<Navigate to="/login" /> } />
+    <Route path='/employee' element={<Employee />} />
+    <Route path='/category' element={<Category />} />
+    <Route path='/profile' element={<Profile />} />
    </Routes>
    </BrowserRouter>
   )
